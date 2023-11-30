95ea42d21efa6e077b2c94dbf76ef843f78c8f26
BagasKelana <hilalbagas@gmail.com>
2023-11-30 15:48:31 +0700

    upd-feat: kick user in Manage Members

--- components/modals/members-modal.tsx
+++ components/modals/members-modal.tsx
@@ -53,6 +53,28 @@ export const MembersModal = () => {
 	const isModalOpen = isOpen && type === 'members';
 	const { server } = data as { server: ServerWithMembersWithProfiles };
 
+	const onKick = async (memberId: string) => {
+		try {
+			setLoadingId(memberId);
+
+			const url = qs.stringifyUrl({
+				url: `/api/members/${memberId}`,
+				query: {
+					serverId: server?.id,
+				},
+			});
+
+			const response = await axios.delete(url);
+
+			router.refresh();
+			onOpen('members', { server: response.data });
+		} catch (error) {
+			console.log(error);
+		} finally {
+			setLoadingId('');
+		}
+	};
+
 	const onRoleChange = async (memberId: string, role: MemberRole) => {
 		try {
 			setLoadingId(memberId);
@@ -146,7 +168,10 @@ export const MembersModal = () => {
 													</DropdownMenuPortal>
 												</DropdownMenuSub>
 												<DropdownMenuSeparator />
-												<DropdownMenuItem>
+												<DropdownMenuItem
+													onClick={() =>
+														onKick(member.id)
+													}>
 													<Gavel className="w-4 h-4 mr-2" />
 													Kick
 												</DropdownMenuItem>
