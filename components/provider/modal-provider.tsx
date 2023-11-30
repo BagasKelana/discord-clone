d598fcd16219cd0b4c37504d78f70e74983dd1c4
BagasKelana <hilalbagas@gmail.com>
2023-11-30 15:34:43 +0700

    feat: manage-members

--- components/provider/modal-provider.tsx
+++ components/provider/modal-provider.tsx
@@ -4,13 +4,14 @@ import { useEffect, useState } from 'react';
 import { CreateServerModal } from '@/components/modals/create-server-modal';
 import { InviteModal } from '@/components/modals/invite-modal';
 import { EditServerModal } from '@/components/modals/edit-server-modal';
+import { MembersModal } from '@/components/modals/members-modal';
 
 export const ModalProvider = () => {
 	const [isMounted, setIsMounted] = useState(false);
 
 	useEffect(() => {
 		setIsMounted(true);
-	}, []); 
+	}, []);
 
 	if (!isMounted) {
 		return null;
@@ -21,6 +22,7 @@ export const ModalProvider = () => {
 			<CreateServerModal />
 			<InviteModal />
 			<EditServerModal />
+			<MembersModal />
 		</>
 	);
 };
