c81a82b25e69827c226a760062df7770c2b149a3
BagasKelana <hilalbagas@gmail.com>
2023-12-23 08:46:40 +0700

    feat: delete channel member

--- components/modals/delete-channel-modal.tsx
+++ components/modals/delete-channel-modal.tsx
@@ -8,28 +8,35 @@ import {
 	DialogHeader,
 	DialogTitle,
 } from '@/components/ui/dialog';
-
+import qs from 'query-string';
 import { useModal } from '@/hooks/use-modal-store';
 import { Button } from '../ui/button';
 import { useState } from 'react';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
-export const DeleteServerModal = () => {
+export const DeleteChannelModal = () => {
 	const { isOpen, onClose, type, data } = useModal();
 	const router = useRouter();
-	const isModalOpen = isOpen && type === 'deleteServer';
-	const { server } = data;
+
+	const isModalOpen = isOpen && type === 'deleteChannel';
+	const { server, channel } = data;
 
 	const [isLoading, setIsLoading] = useState(false);
 
 	const onClick = async () => {
 		try {
 			setIsLoading(true);
+			const url = qs.stringifyUrl({
+				url: `/api/channels/${channel?.id}`,
+				query: {
+					serverId: server?.id,
+				},
+			});
 
-			await axios.delete(`/api/servers/${server?.id}`);
+			await axios.delete(url);
 			onClose();
-			router.push('/');
+			router.push(`/servers/${server?.id}`);
 			router.refresh();
 		} catch (error) {
 			console.log(error);
@@ -44,13 +51,13 @@ export const DeleteServerModal = () => {
 			<DialogContent className="bg-white text-black p-0 overflow-hidden">
 				<DialogHeader className="pt-8 px-6">
 					<DialogTitle className="text-2xl text-center font-bold">
-						Delete Server
+						Delete Channel
 					</DialogTitle>
 					<DialogDescription className="text-center text-zinc-500">
 						Are you sure you want to this? <br />
 						<span className="text-indigo-500 font-semibold">
-							{server?.name}
-						</span>
+							#{channel?.name}
+						</span>{' '}
 						will be permanently deleted
 					</DialogDescription>
 				</DialogHeader>
